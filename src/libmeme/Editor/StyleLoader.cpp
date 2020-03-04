c903eb5b83b83419b479fc1ca0f6ba4cd76e66be
Melody <gurman8r@gmail.com>
2020-03-04 00:48:20 -0500

    StyleLoader.cpp

--- src/libmeme/Editor/StyleLoader.cpp
+++ src/libmeme/Editor/StyleLoader.cpp
@@ -7,127 +7,122 @@ namespace ml
 {
 	bool style_loader::operator()(fs::path const & path)
 	{
+		// open file
 		std::ifstream file{ path };
 		ML_DEFER{ file.close(); };
 		if (!file) return false;
 
-		auto & s{ ImGui::GetStyle() };
-		auto & c{ s.Colors };
+		// style ref
+		auto & style{ ImGui::GetStyle() };
 
+		// scan file
 		pmr::string line;
 		while (std::getline(file, line))
 		{
+			// trim line
 			line = util::trim(line);
 			if (line.empty() || line.front() == '#')
 				continue;
 
+			// tokenize line
 			std::stringstream ss{}; ss << line;
 
+			// scan line
 			auto const tag = input<pmr::string>(ss);
 			auto const name = input<pmr::string>(ss);
-			if (tag == "ImGuiStyle")
+			switch (util::hash(tag))
 			{
-				switch (util::hash(name))
-				{
-				case util::hash("Alpha")					: s.Alpha = input<float_t>(ss); break;
-				case util::hash("WindowPadding")			: s.WindowPadding = input<vec2>(ss); break;
-				case util::hash("WindowRounding")			: s.WindowRounding = input<float_t>(ss); break;
-				case util::hash("WindowBorderSize")			: s.WindowBorderSize = input<float_t>(ss); break;
-				case util::hash("WindowMinSize")			: s.WindowMinSize = input<vec2>(ss); break;
-				case util::hash("WindowTitleAlign")			: s.WindowTitleAlign = input<vec2>(ss); break;
-				case util::hash("ChildRounding")			: s.ChildRounding = input<float_t>(ss); break;
-				case util::hash("ChildBorderSize")			: s.ChildBorderSize = input<float_t>(ss); break;
-				case util::hash("PopupRounding")			: s.PopupRounding = input<float_t>(ss); break;
-				case util::hash("PopupBorderSize")			: s.PopupBorderSize = input<float_t>(ss); break;
-				case util::hash("FramePadding")				: s.FramePadding = input<vec2>(ss); break;
-				case util::hash("FrameRounding")			: s.FrameRounding = input<float_t>(ss); break;
-				case util::hash("FrameBorderSize")			: s.FrameBorderSize = input<float_t>(ss); break;
-				case util::hash("ItemSpacing")				: s.ItemInnerSpacing = input<vec2>(ss); break;
-				case util::hash("ItemInnerSpacing")			: s.ItemInnerSpacing = input<vec2>(ss); break;
-				case util::hash("TouchExtraPadding")		: s.TouchExtraPadding = input<vec2>(ss); break;
-				case util::hash("IndentSpacing")			: s.IndentSpacing = input<float_t>(ss); break;
-				case util::hash("ColumnsMinSpacing")		: s.ColumnsMinSpacing = input<float_t>(ss); break;
-				case util::hash("ScrollbarSize")			: s.ScrollbarSize = input<float_t>(ss); break;
-				case util::hash("ScrollbarRounding")		: s.ScrollbarRounding = input<float_t>(ss); break;
-				case util::hash("GrabMinSize")				: s.GrabMinSize = input<float_t>(ss); break;
-				case util::hash("GrabRounding")				: s.GrabRounding = input<float_t>(ss); break;
-				case util::hash("TabRounding")				: s.TabRounding = input<float_t>(ss); break;
-				case util::hash("TabBorderSize")			: s.TabBorderSize = input<float_t>(ss); break;
-				case util::hash("ButtonTextAlign")			: s.ButtonTextAlign = input<vec2>(ss); break;
-				case util::hash("SelectableTextAlign")		: s.SelectableTextAlign = input<vec2>(ss); break;
-				case util::hash("DisplayWindowPadding")		: s.DisplayWindowPadding = input<vec2>(ss); break;
-				case util::hash("DisplaySafeAreaPadding")	: s.DisplaySafeAreaPadding = input<vec2>(ss); break;
-				case util::hash("MouseCursorScale")			: s.MouseCursorScale = input<float_t>(ss); break;
-				case util::hash("AntiAliasedLines")			: s.AntiAliasedLines = input<bool>(ss); break;
-				case util::hash("AntiAliasedFill")			: s.AntiAliasedFill = input<bool>(ss); break;
-				case util::hash("CurveTessellationTol")		: s.CurveTessellationTol = input<float_t>(ss); break;
-				}
-
-			}
-			else if (tag == "ImGuiCol")
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+			case util::hash("ImGuiStyle"): { switch (util::hash(name))
 			{
-				switch (util::hash(name))
-				{
-				case util::hash("Text")					: c[ImGuiCol_Text] = input<vec4>(ss); break;
-				case util::hash("TextDisabled")			: c[ImGuiCol_TextDisabled] = input<vec4>(ss); break;
-				case util::hash("WindowBg")				: c[ImGuiCol_WindowBg] = input<vec4>(ss); break;
-				case util::hash("ChildBg")				: c[ImGuiCol_ChildBg] = input<vec4>(ss); break;
-				case util::hash("PopupBg")				: c[ImGuiCol_PopupBg] = input<vec4>(ss); break;
-				case util::hash("Border")				: c[ImGuiCol_Border] = input<vec4>(ss); break;
-				case util::hash("BorderShadow")			: c[ImGuiCol_BorderShadow] = input<vec4>(ss); break;
-				case util::hash("FrameBg")				: c[ImGuiCol_FrameBg] = input<vec4>(ss); break;
-				case util::hash("FrameBgHovered")		: c[ImGuiCol_FrameBgHovered] = input<vec4>(ss); break;
-				case util::hash("FrameBgActive")		: c[ImGuiCol_FrameBgActive] = input<vec4>(ss); break;
-				case util::hash("TitleBg")				: c[ImGuiCol_TitleBg] = input<vec4>(ss); break;
-				case util::hash("TitleBgActive")		: c[ImGuiCol_TitleBgActive] = input<vec4>(ss); break;
-				case util::hash("TitleBgCollapsed")		: c[ImGuiCol_TitleBgCollapsed] = input<vec4>(ss); break;
-				case util::hash("MenuBarBg")			: c[ImGuiCol_MenuBarBg] = input<vec4>(ss); break;
-				case util::hash("ScrollbarBg")			: c[ImGuiCol_ScrollbarBg] = input<vec4>(ss); break;
-				case util::hash("ScrollbarGrab")		: c[ImGuiCol_ScrollbarGrab] = input<vec4>(ss); break;
-				case util::hash("ScrollbarGrabHovered")	: c[ImGuiCol_ScrollbarGrabHovered] = input<vec4>(ss); break;
-				case util::hash("ScrollbarGrabActive")	: c[ImGuiCol_ScrollbarGrabActive] = input<vec4>(ss); break;
-				case util::hash("CheckMark")			: c[ImGuiCol_CheckMark] = input<vec4>(ss); break;
-				case util::hash("SliderGrab")			: c[ImGuiCol_SliderGrab] = input<vec4>(ss); break;
-				case util::hash("SliderGrabActive")		: c[ImGuiCol_SliderGrabActive] = input<vec4>(ss); break;
-				case util::hash("Button")				: c[ImGuiCol_Button] = input<vec4>(ss); break;
-				case util::hash("ButtonHovered")		: c[ImGuiCol_ButtonHovered] = input<vec4>(ss); break;
-				case util::hash("ButtonActive")			: c[ImGuiCol_ButtonActive] = input<vec4>(ss); break;
-				case util::hash("Header")				: c[ImGuiCol_Header] = input<vec4>(ss); break;
-				case util::hash("HeaderHovered")		: c[ImGuiCol_HeaderHovered] = input<vec4>(ss); break;
-				case util::hash("HeaderActive")			: c[ImGuiCol_HeaderActive] = input<vec4>(ss); break;
-				case util::hash("Separator")			: c[ImGuiCol_Separator] = input<vec4>(ss); break;
-				case util::hash("SeparatorHovered")		: c[ImGuiCol_SeparatorHovered] = input<vec4>(ss); break;
-				case util::hash("SeparatorActive")		: c[ImGuiCol_SeparatorActive] = input<vec4>(ss); break;
-				case util::hash("ResizeGrip")			: c[ImGuiCol_ResizeGrip] = input<vec4>(ss); break;
-				case util::hash("ResizeGripHovered")	: c[ImGuiCol_ResizeGripHovered] = input<vec4>(ss); break;
-				case util::hash("ResizeGripActive")		: c[ImGuiCol_ResizeGripActive] = input<vec4>(ss); break;
-				case util::hash("Tab")					: c[ImGuiCol_Tab] = input<vec4>(ss); break;
-				case util::hash("TabHovered")			: c[ImGuiCol_TabHovered] = input<vec4>(ss); break;
-				case util::hash("TabActive")			: c[ImGuiCol_TabActive] = input<vec4>(ss); break;
-				case util::hash("TabUnfocused")			: c[ImGuiCol_TabUnfocused] = input<vec4>(ss); break;
-				case util::hash("TabUnfocusedActive")	: c[ImGuiCol_TabUnfocusedActive] = input<vec4>(ss); break;
-				case util::hash("DockingPreview")		: c[ImGuiCol_DockingPreview] = input<vec4>(ss); break;
-				case util::hash("DockingEmptyBg")		: c[ImGuiCol_DockingEmptyBg] = input<vec4>(ss); break;
-				case util::hash("PlotLines")			: c[ImGuiCol_PlotLines] = input<vec4>(ss); break;
-				case util::hash("PlotLinesHovered")		: c[ImGuiCol_PlotLinesHovered] = input<vec4>(ss); break;
-				case util::hash("PlotHistogram")		: c[ImGuiCol_PlotHistogram] = input<vec4>(ss); break;
-				case util::hash("PlotHistogramHovered")	: c[ImGuiCol_PlotHistogramHovered] = input<vec4>(ss); break;
-				case util::hash("TextSelectedBg")		: c[ImGuiCol_TextSelectedBg] = input<vec4>(ss); break;
-				case util::hash("DragDropTarget")		: c[ImGuiCol_DragDropTarget] = input<vec4>(ss); break;
-				case util::hash("NavHighlight")			: c[ImGuiCol_NavHighlight] = input<vec4>(ss); break;
-				case util::hash("NavWindowingHighlight"): c[ImGuiCol_NavWindowingHighlight] = input<vec4>(ss); break;
-				case util::hash("NavWindowingDimBg")	: c[ImGuiCol_NavWindowingDimBg] = input<vec4>(ss); break;
-				case util::hash("ModalWindowDimBg")		: c[ImGuiCol_ModalWindowDimBg] = input<vec4>(ss); break;
-				}
-			}
-
-			if ( !tag.empty())
+			case util::hash("Alpha")					: style.Alpha = input<float_t>(ss); break;
+			case util::hash("WindowPadding")			: style.WindowPadding = input<vec2>(ss); break;
+			case util::hash("WindowRounding")			: style.WindowRounding = input<float_t>(ss); break;
+			case util::hash("WindowBorderSize")			: style.WindowBorderSize = input<float_t>(ss); break;
+			case util::hash("WindowMinSize")			: style.WindowMinSize = input<vec2>(ss); break;
+			case util::hash("WindowTitleAlign")			: style.WindowTitleAlign = input<vec2>(ss); break;
+			case util::hash("ChildRounding")			: style.ChildRounding = input<float_t>(ss); break;
+			case util::hash("ChildBorderSize")			: style.ChildBorderSize = input<float_t>(ss); break;
+			case util::hash("PopupRounding")			: style.PopupRounding = input<float_t>(ss); break;
+			case util::hash("PopupBorderSize")			: style.PopupBorderSize = input<float_t>(ss); break;
+			case util::hash("FramePadding")				: style.FramePadding = input<vec2>(ss); break;
+			case util::hash("FrameRounding")			: style.FrameRounding = input<float_t>(ss); break;
+			case util::hash("FrameBorderSize")			: style.FrameBorderSize = input<float_t>(ss); break;
+			case util::hash("ItemSpacing")				: style.ItemInnerSpacing = input<vec2>(ss); break;
+			case util::hash("ItemInnerSpacing")			: style.ItemInnerSpacing = input<vec2>(ss); break;
+			case util::hash("TouchExtraPadding")		: style.TouchExtraPadding = input<vec2>(ss); break;
+			case util::hash("IndentSpacing")			: style.IndentSpacing = input<float_t>(ss); break;
+			case util::hash("ColumnsMinSpacing")		: style.ColumnsMinSpacing = input<float_t>(ss); break;
+			case util::hash("ScrollbarSize")			: style.ScrollbarSize = input<float_t>(ss); break;
+			case util::hash("ScrollbarRounding")		: style.ScrollbarRounding = input<float_t>(ss); break;
+			case util::hash("GrabMinSize")				: style.GrabMinSize = input<float_t>(ss); break;
+			case util::hash("GrabRounding")				: style.GrabRounding = input<float_t>(ss); break;
+			case util::hash("TabRounding")				: style.TabRounding = input<float_t>(ss); break;
+			case util::hash("TabBorderSize")			: style.TabBorderSize = input<float_t>(ss); break;
+			case util::hash("ButtonTextAlign")			: style.ButtonTextAlign = input<vec2>(ss); break;
+			case util::hash("SelectableTextAlign")		: style.SelectableTextAlign = input<vec2>(ss); break;
+			case util::hash("DisplayWindowPadding")		: style.DisplayWindowPadding = input<vec2>(ss); break;
+			case util::hash("DisplaySafeAreaPadding")	: style.DisplaySafeAreaPadding = input<vec2>(ss); break;
+			case util::hash("MouseCursorScale")			: style.MouseCursorScale = input<float_t>(ss); break;
+			case util::hash("AntiAliasedLines")			: style.AntiAliasedLines = input<bool>(ss); break;
+			case util::hash("AntiAliasedFill")			: style.AntiAliasedFill = input<bool>(ss); break;
+			case util::hash("CurveTessellationTol")		: style.CurveTessellationTol = input<float_t>(ss); break;
+			} } break;
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+			case util::hash("ImGuiCol"): { switch (util::hash(name))
 			{
-				if ( !name.empty())
-				{
-					
-					
-				}
+			case util::hash("Text")					: style.Colors[ImGuiCol_Text] = input<vec4>(ss); break;
+			case util::hash("TextDisabled")			: style.Colors[ImGuiCol_TextDisabled] = input<vec4>(ss); break;
+			case util::hash("WindowBg")				: style.Colors[ImGuiCol_WindowBg] = input<vec4>(ss); break;
+			case util::hash("ChildBg")				: style.Colors[ImGuiCol_ChildBg] = input<vec4>(ss); break;
+			case util::hash("PopupBg")				: style.Colors[ImGuiCol_PopupBg] = input<vec4>(ss); break;
+			case util::hash("Border")				: style.Colors[ImGuiCol_Border] = input<vec4>(ss); break;
+			case util::hash("BorderShadow")			: style.Colors[ImGuiCol_BorderShadow] = input<vec4>(ss); break;
+			case util::hash("FrameBg")				: style.Colors[ImGuiCol_FrameBg] = input<vec4>(ss); break;
+			case util::hash("FrameBgHovered")		: style.Colors[ImGuiCol_FrameBgHovered] = input<vec4>(ss); break;
+			case util::hash("FrameBgActive")		: style.Colors[ImGuiCol_FrameBgActive] = input<vec4>(ss); break;
+			case util::hash("TitleBg")				: style.Colors[ImGuiCol_TitleBg] = input<vec4>(ss); break;
+			case util::hash("TitleBgActive")		: style.Colors[ImGuiCol_TitleBgActive] = input<vec4>(ss); break;
+			case util::hash("TitleBgCollapsed")		: style.Colors[ImGuiCol_TitleBgCollapsed] = input<vec4>(ss); break;
+			case util::hash("MenuBarBg")			: style.Colors[ImGuiCol_MenuBarBg] = input<vec4>(ss); break;
+			case util::hash("ScrollbarBg")			: style.Colors[ImGuiCol_ScrollbarBg] = input<vec4>(ss); break;
+			case util::hash("ScrollbarGrab")		: style.Colors[ImGuiCol_ScrollbarGrab] = input<vec4>(ss); break;
+			case util::hash("ScrollbarGrabHovered")	: style.Colors[ImGuiCol_ScrollbarGrabHovered] = input<vec4>(ss); break;
+			case util::hash("ScrollbarGrabActive")	: style.Colors[ImGuiCol_ScrollbarGrabActive] = input<vec4>(ss); break;
+			case util::hash("CheckMark")			: style.Colors[ImGuiCol_CheckMark] = input<vec4>(ss); break;
+			case util::hash("SliderGrab")			: style.Colors[ImGuiCol_SliderGrab] = input<vec4>(ss); break;
+			case util::hash("SliderGrabActive")		: style.Colors[ImGuiCol_SliderGrabActive] = input<vec4>(ss); break;
+			case util::hash("Button")				: style.Colors[ImGuiCol_Button] = input<vec4>(ss); break;
+			case util::hash("ButtonHovered")		: style.Colors[ImGuiCol_ButtonHovered] = input<vec4>(ss); break;
+			case util::hash("ButtonActive")			: style.Colors[ImGuiCol_ButtonActive] = input<vec4>(ss); break;
+			case util::hash("Header")				: style.Colors[ImGuiCol_Header] = input<vec4>(ss); break;
+			case util::hash("HeaderHovered")		: style.Colors[ImGuiCol_HeaderHovered] = input<vec4>(ss); break;
+			case util::hash("HeaderActive")			: style.Colors[ImGuiCol_HeaderActive] = input<vec4>(ss); break;
+			case util::hash("Separator")			: style.Colors[ImGuiCol_Separator] = input<vec4>(ss); break;
+			case util::hash("SeparatorHovered")		: style.Colors[ImGuiCol_SeparatorHovered] = input<vec4>(ss); break;
+			case util::hash("SeparatorActive")		: style.Colors[ImGuiCol_SeparatorActive] = input<vec4>(ss); break;
+			case util::hash("ResizeGrip")			: style.Colors[ImGuiCol_ResizeGrip] = input<vec4>(ss); break;
+			case util::hash("ResizeGripHovered")	: style.Colors[ImGuiCol_ResizeGripHovered] = input<vec4>(ss); break;
+			case util::hash("ResizeGripActive")		: style.Colors[ImGuiCol_ResizeGripActive] = input<vec4>(ss); break;
+			case util::hash("Tab")					: style.Colors[ImGuiCol_Tab] = input<vec4>(ss); break;
+			case util::hash("TabHovered")			: style.Colors[ImGuiCol_TabHovered] = input<vec4>(ss); break;
+			case util::hash("TabActive")			: style.Colors[ImGuiCol_TabActive] = input<vec4>(ss); break;
+			case util::hash("TabUnfocused")			: style.Colors[ImGuiCol_TabUnfocused] = input<vec4>(ss); break;
+			case util::hash("TabUnfocusedActive")	: style.Colors[ImGuiCol_TabUnfocusedActive] = input<vec4>(ss); break;
+			case util::hash("DockingPreview")		: style.Colors[ImGuiCol_DockingPreview] = input<vec4>(ss); break;
+			case util::hash("DockingEmptyBg")		: style.Colors[ImGuiCol_DockingEmptyBg] = input<vec4>(ss); break;
+			case util::hash("PlotLines")			: style.Colors[ImGuiCol_PlotLines] = input<vec4>(ss); break;
+			case util::hash("PlotLinesHovered")		: style.Colors[ImGuiCol_PlotLinesHovered] = input<vec4>(ss); break;
+			case util::hash("PlotHistogram")		: style.Colors[ImGuiCol_PlotHistogram] = input<vec4>(ss); break;
+			case util::hash("PlotHistogramHovered")	: style.Colors[ImGuiCol_PlotHistogramHovered] = input<vec4>(ss); break;
+			case util::hash("TextSelectedBg")		: style.Colors[ImGuiCol_TextSelectedBg] = input<vec4>(ss); break;
+			case util::hash("DragDropTarget")		: style.Colors[ImGuiCol_DragDropTarget] = input<vec4>(ss); break;
+			case util::hash("NavHighlight")			: style.Colors[ImGuiCol_NavHighlight] = input<vec4>(ss); break;
+			case util::hash("NavWindowingHighlight"): style.Colors[ImGuiCol_NavWindowingHighlight] = input<vec4>(ss); break;
+			case util::hash("NavWindowingDimBg")	: style.Colors[ImGuiCol_NavWindowingDimBg] = input<vec4>(ss); break;
+			case util::hash("ModalWindowDimBg")		: style.Colors[ImGuiCol_ModalWindowDimBg] = input<vec4>(ss); break;
+			}} break;
+			/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 			}
 		}
 		return true;
