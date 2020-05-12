3cdf5ef2382fa98fd7322e805f0336bdf815ecfb
Melody <gurman8r@gmail.com>
2020-05-12 03:12:55 -0400

    inline!

--- include/libmeme/Core/PerformanceTracker.hpp
+++ include/libmeme/Core/PerformanceTracker.hpp
@@ -40,7 +40,7 @@ namespace ml
 		{
 			cstring const id;
 
-			template <class Fn> auto operator+(Fn && fn) const noexcept
+			template <class Fn> inline auto operator+(Fn && fn) const noexcept
 			{
 				return benchmark<Fn>{ id, ML_forward(fn) };
 			}
